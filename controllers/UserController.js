fb46c4e91d4c4439cc34c88466defaf13a511f1e
asg1998f <asg1998f@gmail.com>
2024-11-19 17:37:37 +0100

    endpoint para crear usuario creado

---
+++ controllers/UserController.js
@@ -0,0 +1,15 @@
+const { User } = require("../models/index")
+
+const UserController ={
+    async create(req,res){
+        try{
+            const user = await User.create(req.body)
+            res.status(201).send({message:"User created successfully",user})
+        } catch (error){
+            console.error(error);
+            res.status(500).send({message:"There was a problem",error})
+        }
+    }
+}
+
+module.exports = UserController
