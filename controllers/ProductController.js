3b1f5da6273eefddaa62a8f4c13cede1947fc7e4
asg1998f <asg1998f@gmail.com>
2024-11-24 20:35:27 +0100

    CRUD products hecho

---
+++ controllers/ProductController.js
@@ -0,0 +1,56 @@
+const { Product } = require("../models/index")
+
+const ProductController = {
+    async create(req,res){
+        try {
+            const product = await Product.create(req.body)
+            res.status(201).send({message:"Product created successfully",product})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message:"There was a problem",error})
+        }
+    },
+    async getByID(req,res){
+        try {
+            const product = await Product.findByPk(req.params.id);
+            res.send(product)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message:"There was a problem",error})
+
+        }
+        
+    },
+    async update(req,res){
+        try {  await Product.update(req.body,
+            {
+                where: {
+                    id: req.params.id
+                }
+            })
+         res.send({message:"Product successfully updated"})
+   
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({message:"There was a problem",error})
+        }
+      
+     },
+    async delete(req,res){
+        try {
+            await Product.destroy({
+                where:{
+                    id: req.params.id
+                }
+            })
+            res.send({message:`Product with id ${req.params.id} deleted`})
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({message:"There was a problem",error})
+        }
+      
+    }
+
+}
+
+module.exports = ProductController
---
+++ routes/products.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const ProductController = require("../controllers/ProductController");
+const router = express.Router()
+
+router.post("/products",ProductController.create)
+router.put("/id/:id",ProductController.update)
+router.delete("/id/:id",ProductController.delete)
+
+module.exports = router
